179fde7a4fdac421798d4dd743ded761037a7ca2
breadbread1984 <breadbread1984@163.com>
2024-02-08 09:37:16 +0800

    update

--- models.py
+++ models.py
@@ -24,9 +24,9 @@ class ContinuousFilterConvolution(tf.keras.layers.Layer):
     self.bias5 = self.add_weight(name = 'bias5', shape = (self.channels,), initializer = tf.keras.initializers.GlorotUniform(), trainable = True)
   def call(self, graph, edge_set_name):
     # 1) filternet
-    receiver_positions = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.TARGET, feature_name = "position") # recevier_position.shape = (edge_num, 3)
     sender_positions = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.SOURCE, feature_name = "position") # sender_position.shape = (edge_num, 3)
-    dists = tf.math.sqrt(tf.math.reduce_sum((receiver_positions - sender_positinos) ** 2, axis = -1, keepdims = True)) # dists.shape = (edge_num, 1)
+    receiver_positions = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.TARGET, feature_name = "position") # recevier_position.shape = (edge_num, 3)
+    dists = tf.math.sqrt(tf.math.reduce_sum((sender_positinos - receiver_positions) ** 2, axis = -1, keepdims = True)) # dists.shape = (edge_num, 1)
     centers = tf.expand_dims(tf.linspace(0., self.cutoff, int(tf.math.ceil(self.cutoff / self.gap))), axis = 0) # centers.shape = (1, center_num)
     dists = dists - centers # dists.shape = (edge_num, center_num)
     rbf = tf.math.exp(-(dists ** 2) / self.gap) # rbf.shape = (edge_num, center_num)
@@ -34,11 +34,11 @@ class ContinuousFilterConvolution(tf.keras.layers.Layer):
     results = self.shifted_softplus(tf.linalg.matmul(rbf, self.weight1) + self.bias1) # results.shape = (edge_num, channels)
     w = self.shifted_softplus(tf.linalg.matmul(results, self.weight2) + self.bias2) # results.shape = (edge_num, channels)
     # 2) continuous fileter convolution
-    x = tfgnn.keras.layers.Readout(node_set_name = tfgnn.HIDDEN_STATE) # x.shape = (node_num, channels)
+    x = tfgnn.keras.layers.Readout(node_set_name = 'atom', feature_name = tfgnn.HIDDEN_STATE)(graph) # x.shape = (node_num, channels)
     f = tf.linalg.matmul(x, self.weight3) # results.shape = (node_num, channels)
     f = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.SOURCE, feature_value = f) # receiver_states.shape = (edge_num, channels)
     wf = w * f # wf.shape = (node_num, channels)
-    conv = tfgnn.pool_edges_to_node(graph, edge_set_name, tfgnn.TARGET, 'sum', wf) # conv.shape = (node_num, channels)
+    conv = tfgnn.pool_edges_to_node(graph, edge_set_name, tfgnn.SOURCE, reduce_type = 'sum', feature_value = wf) # conv.shape = (node_num, channels)
     y = self.shifted_softplus(tf.linalg.matmul(conv, self.weight4) + self.bias4) # y.shape = (node_num, channels)
     # 3) dense
     v = tf.linalg.matmul(y, self.weight5) + self.bias5 # v.shape = (node_num, channels)
