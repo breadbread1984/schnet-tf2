f5534f5b9f4079ec5c1f759a6bede39856d48f86
breadbread1984 <breadbread1984@163.com>
2024-02-09 12:05:50 +0800

    update

--- models.py
+++ models.py
@@ -10,10 +10,11 @@ class FilterNet(tf.keras.layers.Layer):
   def __init__(self, **kwargs):
     self.cutoff = kwargs.get('cutoff', 20.)
     self.gap = kwargs.get('gap', 0.1)
-  def call(self, graph, edge_set_name):
+  def call(self, inputs):
     # 1) filternet
-    sender_positions = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.SOURCE, feature_name = "position") # sender_position.shape = (edge_num, 3)
-    receiver_positions = tfgnn.broadcast_node_to_edges(graph, edge_set_name, tfgnn.TARGET, feature_name = "position") # recevier_position.shape = (edge_num, 3)
+    sender_positions = inputs[tfgnn.SOURCE] # sender_position.shape = (edge_num, 3)
+    receiver_positions = inputs[tfgnn.TARGET] # receiver_positions.shape = (edge_num, 3)
+    # 1) filternet
     offsets = tfgnn.keras.layers.Readout(edge_set_name = 'bond', feature_name = 'offset')(graph) # offsets.shape = (edge_num, 3)
     dists = tf.math.sqrt(tf.math.reduce_sum((sender_positinos - (receiver_positions + offsets)) ** 2, axis = -1, keepdims = True)) # dists.shape = (edge_num, 1)
     centers = tf.expand_dims(tf.linspace(0., self.cutoff, int(tf.math.ceil(self.cutoff / self.gap))), axis = 0) # centers.shape = (1, center_num)
@@ -47,7 +48,7 @@ class ContinuousFilterConvolution(tf.keras.layers.Layer):
     self.weight5 = self.add_weight(name = 'weight5', shape = (self.channels, self.channels), initializer = tf.keras.initializers.GlorotUniform(), trainable = True)
     self.bias5 = self.add_weight(name = 'bias5', shape = (self.channels,), initializer = tf.keras.initializers.GlorotUniform(), trainable = True)
   def call(self, graph, edge_set_name):
-    rbf = tfgnn.keras.layers.Readout(edge_set_name = 'bond', feature_name = 'rbf')(graph) # rbf.shape = (edge_num, center_num)
+    rbf = tfgnn.keras.layers.Readout(edge_set_name = edge_set_name, feature_name = 'rbf')(graph) # rbf.shape = (edge_num, center_num)
     results = shifted_softplus(tf.linalg.matmul(rbf, self.weight1) + self.bias1) # results.shape = (edge_num, channels)
     w = shifted_softplus(tf.linalg.matmul(results, self.weight2) + self.bias2) # results.shape = (edge_num, channels)
     # 2) continuous fileter convolution
@@ -78,15 +79,16 @@ def SchNet(channels = 256, layer_num = 4):
     node_sets_fn = lambda node_set, *, node_set_name: tf.keras.layers.Dense(channels)(node_set[tfgnn.HIDDEN_STATE]))(graph)
   # update context
   graph = tfgnn.keras.layers.GraphUpdate(
-    context = tfgnn.keras.layers.EdgeSetUpdate(
-      edge_sets = {
-        "rbf": FilterNet()
-      },
-      next_state = tfgnn.keras.layers.NextStateFromConcat(
-        transformation = tf.keras.layers.Identity()
+    edge_sets = {
+      "bond": tfgnn.keras.layers.EdgeSetUpdate(
+        node_input_feature = "position",
+        node_input_tags = (tfgnn.SOURCE, tfgnn.TARGET),
+        next_state = tfgnn.keras.layers.NextStateFromConcat(
+          transformation = FilterNet()
+        )
       )
-    )
-  )
+    }
+  )(graph)
   # update node
   for i in range(layer_num):
     graph = tfgnn.keras.layers.GraphUpdate(
