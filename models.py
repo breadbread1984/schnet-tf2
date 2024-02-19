2b521616d8451721240a948f68a5ff87536aa908
breadbread1984 <breadbread1984@163.com>
2024-02-19 16:00:32 +0800

    update

--- models.py
+++ models.py
@@ -113,4 +113,4 @@ def SchNet(channels = 256, layer_num = 4):
   results = tf.keras.layers.Dense(channels // 2, activation = shifted_softplus)(results) # results.shape = (batch, channels // 2)
   results = tf.keras.layers.Dense(len(prop_names))(results) # results.shape = (batch, len(prop_names))
   results = tf.keras.layers.Lambda(lambda x, n: tf.split(x, n, axis = -1), arguments = {'n': len(prop_names)})(results)
-  return tf.keras.Model(inputs = inputs, outputs = {k:v for k,v in zip(prop_names,results)})
+  return tf.keras.Model(inputs = graph, outputs = {k:v for k,v in zip(prop_names,results)})
